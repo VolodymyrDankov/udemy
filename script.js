e63af03bc0a53154b34b383dfb9261112a29af1b
Volodymyr <volodymyr.dankov@gmail.com>
2020-01-04 03:12:12 +0200

    update js

---
+++ script.js
@@ -0,0 +1,36 @@
+//let nut = 30;
+
+//if (nut == 30) {
+//    console.log("Верно")
+//} else {
+//    console.log("Не верно")
+//}
+
+let numm = 20;
+let nun = 9;
+
+switch (nun) {
+    case nun < 60:
+        console.log("Много");
+        break;
+    case nun > 40:
+        console.log("Мфло ");
+        break;
+    case 50:
+        console.log("Верно");
+        break;
+    case nun > 100:
+        console.log("Очень много");
+        break;
+    default:
+        console.log("Вапше не то");
+        break;
+}
+
+let run = 9;
+
+if (run < 10) {
+    console.log("Nice");
+} else {
+    console.log("Not good");
+ }
